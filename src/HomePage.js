07551ebe404523016bc7ceffdb5e8d123cd67856
Shanu1sn0s <105343717+Shanu1sn0s@users.noreply.github.com>
2022-11-05 23:43:05 +0800

    2 files modified

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,38 +1,51 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import {Helmet} from 'react-helmet-async'
+import { Link } from 'react-router-dom';
+import { Helmet } from 'react-helmet-async';
 
 const Home = () => {
-
-  return(
-    
-    <div className = 'container'>
-      
+  return (
+    <div className="container">
       <Helmet>
-      <title>Home</title>
-      <meta name='description' content='Counter home page with access to 404page, error page, usereducer counter and custom counter page'/>
-      <link rel='canonical' href='/'/>
-    </Helmet>
-        <h1>Home Page</h1>
-       <h2>Alschool Examination</h2>
+        <title>Home</title>
+        <meta
+          name="description"
+          content="Counter home page with access to 404page, error page, usereducer counter and custom counter page"
+        />
+        <link rel="canonical" href="/" />
+      </Helmet>
+      <h1>Home Page</h1>
+      <h2>Alschool Examination</h2>
+      <hr />
       <h5>Created by : Shanu Olayinka</h5>
-      <div className = 'links home' >
+      <div className="links home">
         <div>
-          <Link to="/404" className= 'link'>404</Link>
+          <Link to="/404" className="link">
+            404
+          </Link>
         </div>
         <div>
-          <Link to="/error+boundary" className= 'link'>Error</Link>
+          <Link to="/error+boundary" className="link">
+            Error
+          </Link>
         </div>
         <div>
-          <Link to="/use+reducer" className= 'link'>To useReducer</Link>
+          <Link to="/use+reducer" className="link">
+            To useReducer
+          </Link>
         </div>
         <div>
-          <Link to="/counter" className= 'link'>Custom Counter Page</Link>
+          <Link to="/counter" className="link">
+            Custom Counter Page
+          </Link>
+        </div>
+        <div className= 'box'>
+          <span>
+            <h3>Thank you!!!</h3>
+          </span>
         </div>
       </div>
-     
     </div>
-  )
-}
+  );
+};
 
 export default Home;
